fa1e8836ef7bc9242f9b34b470a19d15c84c982b
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2017-03-14 12:33:09 +0100

    origin
    Commit inicial

---
+++ Actividad_1Login/Actividad_1Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Actividad_1Login
+//
+//  Created by Sergio Redondo on 14/3/17.
+//  Copyright © 2017 Sergio Redondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
