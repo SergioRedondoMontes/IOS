8cf3973692c4a6f17c1a2755f2fb647a22b7ffcf
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2017-03-22 17:59:25 +0100

    Pantalla principal
    Funciones ir a registro
    Creación de TAB BAR
    Transición manual a TAB BAR
    Transición auto volver a inicio

---
+++ Actividad_1Login/Actividad_1Login/VCRegistro.swift
@@ -0,0 +1,38 @@
+//
+//  VCRegistro.swift
+//  Actividad_1Login
+//
+//  Created by Sergio Redondo on 22/3/17.
+//  Copyright © 2017 Sergio Redondo. All rights reserved.
+//
+
+import UIKit
+
+class VCRegistro: UIViewController {
+    
+    @IBOutlet var txtRUsuario:UITextField?
+    @IBOutlet var txtRPass:UITextField?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Actividad_1Login/Actividad_1Login/ViewController.swift
+++ Actividad_1Login/Actividad_1Login/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var btnLogin:UIButton?
+    @IBOutlet var txtUsuario:UITextField?
+    @IBOutlet var txtPass:UITextField?
+    @IBOutlet var lblConsola:UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func accionBtnLogear() {
+       // lblConsola?.text=String(format:"Usuario o contraseña no validos")
+        
+        
+        if txtUsuario?.text=="a" && txtPass?.text=="a" {
+            self.performSegue(withIdentifier: "trans1", sender: self)
+        }else{
+            lblConsola?.text=String(format:"Usuario o contraseña no validos")
+        }
+    }
 }
 
